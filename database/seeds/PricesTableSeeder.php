d632747aa145c82e76ccb65d95a66176ad3d0481
diegosilva91 <diegoarturosilva@hotmail.com>
2020-03-24 09:00:06 +0100

    update seeders for production

--- database/seeds/PricesTableSeeder.php
+++ database/seeds/PricesTableSeeder.php
@@ -14,10 +14,11 @@ class PricesTableSeeder extends Seeder
     public function run()
     {
         //
+
         $fake = Faker::create();
-        for($i = 0; $i<20; $i++) {
+        for($i = 1; $i<20; $i++) {
             $current_quantity = $fake->randomFloat(1,0,100);
-            $action_id=$fake->numberBetween(1,20);
+            $action_id=$i*10+1;
             \DB::table('prices')->insert(array(
                 'current_quantity' => $current_quantity,
                 'actions_id'=>$action_id,
