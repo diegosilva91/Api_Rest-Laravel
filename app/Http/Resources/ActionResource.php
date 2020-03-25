b2e33cb773787a6d81f1ccc4b45f17e065cbaaa0
diegosilva91 <diegoarturosilva@hotmail.com>
2020-03-25 19:59:44 +0100

    Actions resource for api

---
+++ app/Http/Resources/ActionResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ActionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name'=>$this->name,
+            'unique_code'=>$this->unique_code,
+            'description'=>$this->description,
+            'logo'=>$this->logo,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+            'price' => $this->price,
+        ];
+        //return parent::toArray($request);
+    }
+}
