654bacfab75fde84bf58b8ecd0659df9f62e0d3f
diegosilva91 <diegoarturosilva@hotmail.com>
2020-03-30 03:50:42 +0200

    Revert commits for production test, test failed

--- app/Actions.php
+++ app/Actions.php
@@ -14,24 +14,7 @@ class Actions extends Model
     }
     public function Candle($value)
     {
-        $subquery=\DB::table('prices')
-            ->select(\DB::raw('actions_id,
-		   DATE(created_at) AS dates,
-           MIN(current_quantity) AS min_value,
-           MAX(current_quantity) AS max_value,
-           MIN(created_at) AS min_created,
-           MAX(created_at) AS max_created'))
-            ->whereRaw('actions_id='.$value)
-            ->groupBy('dates','actions_id')
-            ->toSql();
-        $query=\DB::table(\DB::raw('('.$subquery.') AS t'))
-            ->select(\DB::raw('t.dates, t.min_value,t.max_value,
-                FIRST_VALUE(price_min.current_quantity) OVER(ORDER BY price_min.created_at DESC) AS open_value,
-                LAST_VALUE(price_max.current_quantity) OVER(ORDER BY price_min.created_at ASC) AS close_value
-            '))
-            ->join('prices AS price_min', 'price_min.created_at', '=', 't.min_created')
-            ->join('prices AS price_max', 'price_max.created_at', '=', 't.max_created');
-        return $query->get();
+
     }
 
 }
