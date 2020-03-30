2fa2e0144e0960835aa8ad23fb67c3305c3d7441
diegosilva91 <diegoarturosilva@hotmail.com>
2020-03-30 11:01:57 +0200

    spit in select differents

--- app/Actions.php
+++ app/Actions.php
@@ -25,10 +25,8 @@ class Actions extends Model
             ->groupBy('dates','actions_id')
             ->toSql();
         $query=\DB::table(\DB::raw('('.$subquery.') AS t'))
-            ->select(\DB::raw('t.dates, t.min_value,t.max_value,
-                FIRST_VALUE(price_min.current_quantity) OVER(ORDER BY price_min.created_at DESC) open_value,
-                LAST_VALUE(price_max.current_quantity) OVER(ORDER BY price_max.created_at ASC) close_value
-            '))
+            ->select('t.dates', 't.min_value','t.max_value',\DB::raw('FIRST_VALUE(price_min.current_quantity) OVER(ORDER BY price_min.created_at DESC) open_value,
+                LAST_VALUE(price_max.current_quantity) OVER(ORDER BY price_max.created_at ASC) close_value'))
             ->join('prices AS price_min', 'price_min.created_at', '=', 't.min_created')
             ->join('prices AS price_max', 'price_max.created_at', '=', 't.max_created');
         return $query->get();
